dcb5a2b20039ae7b72e7453954281fc2160e2c29
Doug Kintop <dkintop@hotmail.com>
2019-09-09 19:58:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,10 @@
+require 'pry'
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!" && phrase == phrase.upcase
      "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA"
    "NO, NOT SINCE 1938!"
+  binding.pry
   end
 end
 
